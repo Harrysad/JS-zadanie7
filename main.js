aae41ce8733e098bf77211e6623bdc259985a36b
sdebowski <s_debowski@wp.pl>
2024-06-25 15:32:03 +0200

    WYkonanie zadania

---
+++ main.js
@@ -0,0 +1,10 @@
+var firstNumber = 5;
+var secondNumber = 10;
+
+function suma(a, b) {
+    var result = a + b;
+    return result;
+}
+
+var wynik = suma(firstNumber, secondNumber);
+console.log("Wynik dodawania:", wynik);
